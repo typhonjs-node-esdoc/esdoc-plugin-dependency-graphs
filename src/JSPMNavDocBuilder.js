0e2a04860ed531566d7e3157adb44a16ac1d6622
typhonrt <support@typhonrt.org>
2016-04-16 14:40:38 -0700

    eslint fixes for JSPMNavDocBuilder

--- src/JSPMNavDocBuilder.js
+++ src/JSPMNavDocBuilder.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import cheerio    from 'cheerio';
 import fs         from 'fs-extra';
 import path       from 'path';
 
@@ -20,36 +19,43 @@ export default class JSPMNavDocBuilder extends DocBuilder
     */
    buildNavDoc()
    {
-      let html = this._readTemplate('nav.html');
-      let ice = new IceCap(html);
+      const html = this._readTemplate('nav.html');
+      const ice = new IceCap(html);
 
       const kinds = ['class', 'function', 'variable', 'typedef', 'external'];
-      const allDocs = this._find({kind: kinds}).filter(v => !v.builtinExternal);
-      const kindOrder = {class: 0, interface: 1, function: 2, variable: 3, typedef: 4, external: 5};
-      allDocs.sort((a, b)=>{
+      const allDocs = this._find({ kind: kinds }).filter((v) => !v.builtinExternal);
+      const kindOrder = { 'class': 0, 'interface': 1, 'function': 2, 'variable': 3, 'typedef': 4, 'external': 5 };
+
+      allDocs.sort((a, b) =>
+      {
          const filePathA = a.longname.split('~')[0].replace('src/', '');
          const filePathB = b.longname.split('~')[0].replace('src/', '');
          const dirPathA = path.dirname(filePathA);
          const dirPathB = path.dirname(filePathB);
          const kindA = a.interface ? 'interface' : a.kind;
          const kindB = b.interface ? 'interface' : b.kind;
-         if (dirPathA === dirPathB) {
-            if (kindA === kindB) {
-               return a.longname > b.longname ? 1 : -1;
-            } else {
-               return kindOrder[kindA] > kindOrder[kindB] ? 1 : -1;
-            }
-         } else {
+
+         if (dirPathA === dirPathB)
+         {
+            if (kindA === kindB) { return a.longname > b.longname ? 1 : -1; }
+            else { return kindOrder[kindA] > kindOrder[kindB] ? 1 : -1; }
+         }
+         else
+         {
             return dirPathA > dirPathB ? 1 : -1;
          }
       });
+
       let lastDirPath = '.';
-      ice.loop('doc', allDocs, (i, doc, ice)=>{
+
+      ice.loop('doc', allDocs, (i, doc, ice) =>
+      {
          const filePath = doc.longname.split('~')[0].replace(/^.*?[/]/, '');
          const dirPath = path.dirname(filePath);
          const kind = doc.interface ? 'interface' : doc.kind;
          const kindText = kind.charAt(0).toUpperCase();
          const kindClass = `kind-${kind}`;
+
          ice.load('name', this._buildDocLinkHTML(doc.longname));
          ice.load('kind', kindText);
          ice.attr('kind', 'class', kindClass);
